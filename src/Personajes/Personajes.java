1034bc949207fb25247ca3b1be4252d23408f482
mar77in <martinchom88@gmail.com>
2017-09-24 17:27:40 -0300

    cambie la letra ñ que me rompe el codigo

--- src/Personajes/Personajes.java
+++ src/Personajes/Personajes.java
@@ -9,7 +9,7 @@ public abstract class Personajes extends gameObject {
 
 	protected Icon[] graficos;
 	protected int resistencia;
-	protected int daño;
+	protected int danio;
 	protected int rango;
 	private volatile  boolean moviendo=false;
 	
@@ -20,8 +20,8 @@ public abstract class Personajes extends gameObject {
 		return resistencia;
 	}
 	
-	public int getDaño(){
-		return daño;
+	public int getDanio(){
+		return danio;
 	}
 	
 	
